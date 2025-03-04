6b0574c1d3122eafd2d4f4625a6d65161e6d54f6
o2196215 <emma.ottmann@etu.uni-orleans.fr>
2025-03-04 11:51:19 +0100

    init commit

---
+++ src/main/java/fr/miage/syp/chellengesgroupe9/ChellengesGroupe9Application.java
@@ -0,0 +1,13 @@
+package fr.miage.syp.chellengesgroupe9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChellengesGroupe9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChellengesGroupe9Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/miage/syp/chellengesgroupe9/ChellengesGroupe9ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.syp.chellengesgroupe9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChellengesGroupe9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
